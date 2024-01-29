8eb43ea90b853c86d6615bbfa18bd5b805bfb059
VarNotUsed <varnotused@protonmail.com>
2024-01-29 14:14:02 +0100

    base setup

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,26 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Whelp Landing`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.whelp-hub.io`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-material-ui",
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+  ],
+};
 
-export default config
+export default config;
---
+++ src/layout/index.tsx
@@ -0,0 +1,127 @@
+import React from "react";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+
+import Navbar from "../components/Navbar";
+//import Footer from "../components/Footer";
+
+import "@fontsource/inter";
+import "@fontsource/inter/400.css";
+import "./style.css";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#44CA83"
+    }
+  },
+  typography: {
+    button: {
+      color: "#0A0C0F",
+      fontFamily: "Inter",
+      fontSize: "16px",
+      fontStyle: "normal",
+      fontWeight: 500,
+      lineHeight: "24px",
+      textTransform: "none"
+    },
+    h1: {
+      color: "#FFF", 
+      fontFamily: "Inter",
+      fontSize: "32px",
+      fontStyle: "normal",
+      fontWeight: 600,
+      lineHeight: "40px",
+      "@media (min-width:900px)": {
+        fontSize: "60px",
+        lineHeight: "64px",
+      },
+    },
+    h2: {
+      color: "#FFF",
+      fontFamily: "Inter",
+      fontSize: "24px",
+      fontStyle: "normal",
+      fontWeight: 600,
+      lineHeight: "32px",
+      "@media (min-width:900px)": {
+        fontSize: "44px",
+        lineHeight: "52px",
+      },
+    },
+    h3: {
+      fontFamily: "Inter",
+      fontSize: "24px",
+      fontStyle: "normal",
+      fontWeight: 600,
+      lineHeight: "32px",
+      "@media (min-width:900px)": {
+        fontSize: "36px",
+        lineHeight: "44px",
+      },
+    },
+    body1: {
+      color: "rgba(255, 255, 255, 0.76)",
+      fontFamily: "Inter",
+      fontSize: "16px",
+      fontStyle: "normal",
+      fontWeight: 400,
+      lineHeight: "24px"
+    }
+  },
+  components: {
+    MuiContainer: {
+      styleOverrides: {
+        maxWidthSm: {
+          "&.MuiContainer-maxWidthSm": {
+            maxWidth: 200,
+          },
+        },
+        maxWidthMd: {
+          "&.MuiContainer-maxWidthMd": {
+            maxWidth: 320,
+          },
+        },
+        maxWidthLg: {
+          "&.MuiContainer-maxWidthLg": {
+            maxWidth: 1310,
+          },
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: "10px",
+          border: "1px solid rgba(255, 255, 255, 0.10)",
+          background:
+            "var(--Whelp-button-primary-bg, linear-gradient(180deg, rgba(255, 255, 255, 0.32) 0%, rgba(255, 255, 255, 0.00) 100%), #44CA83)",
+          textTransform: "none",
+          boxShadow: "none"
+        },
+      },
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+  },
+  spacing: 8,
+});
+
+const Layout = ({ children }: { children: any }) => {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        {children}
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default Layout;
