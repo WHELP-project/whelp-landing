02b9a7616b7ef2f10337d0eac0464ac29ffd5f60
VarNotUsed <varnotused@protonmail.com>
2024-02-01 15:25:48 +0100

    text image animation

--- src/components/TextImage/index.tsx
+++ src/components/TextImage/index.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import Divider from "../../assets/Divider.svg";
 import TextImageBg1 from "../../images/TextImageBg1.png";
 import TextImageBg2 from "../../images/TextImageBg2.png";
+import { Fade, Slide } from "react-awesome-reveal";
 
 const ButtonStyle = {
   borderRadius: "12px",
@@ -11,18 +12,20 @@ const ButtonStyle = {
   padding: "12px 24px",
   width: {
     xs: "100%",
-    md: "unset"
-  }
+    md: "unset",
+  },
 };
 
 const TextImage = () => {
   return (
-    <Box sx={{
-      mb: {
-        xs: 12,
-        md: 24
-      }
-    }}>
+    <Box
+      sx={{
+        mb: {
+          xs: 12,
+          md: 24,
+        },
+      }}
+    >
       <Container>
         <Box
           sx={{
@@ -37,44 +40,56 @@ const TextImage = () => {
         >
           <Grid container spacing={3}>
             <Grid item xs={12} md={6}>
-              <Typography variant="h2" mb={3}>
-                Swap tokens fast and easy
-              </Typography>
-              <Typography variant="body1" mb={3}>
-                Lorem ipsum dolor sit amet consectetur. Cursus egestas sit purus
-                id risus enim eu porttitor. Enim morbi orci sit turpis. Nulla
-                pulvinar suspendisse nibh commodo posuere fermentum sed. Vitae
-                mi massa erat consequat cursus.
-              </Typography>
-              <Button sx={ButtonStyle}>Learn More</Button>
+              <Fade direction="left">
+                <Typography variant="h2" mb={3}>
+                  Swap tokens fast and easy
+                </Typography>
+                <Typography variant="body1" mb={3}>
+                  Lorem ipsum dolor sit amet consectetur. Cursus egestas sit
+                  purus id risus enim eu porttitor. Enim morbi orci sit turpis.
+                  Nulla pulvinar suspendisse nibh commodo posuere fermentum sed.
+                  Vitae mi massa erat consequat cursus.
+                </Typography>
+                <Button sx={ButtonStyle}>Learn More</Button>
+              </Fade>
             </Grid>
             <Grid item xs={12} md={6}>
-              <Box component="img" src={TextImageBg1} />
+              <Fade direction="right">
+                <Box component="img" src={TextImageBg1} />
+              </Fade>
             </Grid>
           </Grid>
-          
-          <Box mb={3} mt={2} sx={{
-            maxWidth: "100%",
-            overflowX: "hidden"
-          }}>
+
+          <Box
+            mb={3}
+            mt={2}
+            sx={{
+              maxWidth: "100%",
+              overflowX: "hidden",
+            }}
+          >
             <Divider />
           </Box>
 
           <Grid container spacing={3} className="reverseGrid">
             <Grid item xs={12} md={6}>
-              <Box component="img" src={TextImageBg1} />
+              <Fade direction="left">
+                <Box component="img" src={TextImageBg1} />
+              </Fade>
             </Grid>
             <Grid item xs={12} md={6}>
-              <Typography variant="h2" mb={3}>
-                Swap tokens fast and easy
-              </Typography>
-              <Typography variant="body1" mb={3}>
-                Lorem ipsum dolor sit amet consectetur. Cursus egestas sit purus
-                id risus enim eu porttitor. Enim morbi orci sit turpis. Nulla
-                pulvinar suspendisse nibh commodo posuere fermentum sed. Vitae
-                mi massa erat consequat cursus.
-              </Typography>
-              <Button sx={ButtonStyle}>Learn More</Button>
+              <Fade direction="right">
+                <Typography variant="h2" mb={3}>
+                  Swap tokens fast and easy
+                </Typography>
+                <Typography variant="body1" mb={3}>
+                  Lorem ipsum dolor sit amet consectetur. Cursus egestas sit
+                  purus id risus enim eu porttitor. Enim morbi orci sit turpis.
+                  Nulla pulvinar suspendisse nibh commodo posuere fermentum sed.
+                  Vitae mi massa erat consequat cursus.
+                </Typography>
+                <Button sx={ButtonStyle}>Learn More</Button>
+              </Fade>
             </Grid>
           </Grid>
         </Box>
